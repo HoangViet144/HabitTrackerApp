d319a0bc05e402cc9b9606c75befcb3b45bf728e
Trần Duy Minh <92656809+Mdtr3002@users.noreply.github.com>
2021-12-08 00:08:39 +0700

    Habit tracker Dashboard
    Dashboard page shows information of active goal

--- lib/screen/dashboard/index.dart
+++ lib/screen/dashboard/index.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 import 'package:habit_tracker_app/component/my_drawer.dart';
 import 'package:habit_tracker_app/model/goal.dart';
 import 'package:habit_tracker_app/model/index.dart';
@@ -16,17 +17,126 @@ class DashBoardScreen extends StatelessWidget {
         actions: [
           ElevatedButton(
             onPressed: () => _tmp(context),
-            child: const Text("Text add goal"),
+            child: Row(
+              children: [
+                const Text("Add goal"),
+                Icon(Icons.add_circle_outline,
+                  size:20,
+                ),
+              ],
+            )
           )
         ],
       ),
-      body: ListView.builder(
-        itemCount: AppStateScope.of(context).goalList.length,
-        itemBuilder: (context, i) {
-          return const Text("Goal here");
-        },
-      ),
       drawer: const MyDrawer(),
+      body:
+          HomePage(),
     );
   }
 }
+class HomePage extends StatelessWidget {
+  final List<Color> colors =<Color>[Colors.red,Colors.blue,Colors.orange,Colors.green,Colors.yellow,Colors.pink,Colors.brown,Colors.purple];
+  @override
+  Widget build(BuildContext context) {
+    return
+      ListView.separated(
+        padding: const EdgeInsets.all(20),
+        itemCount: AppStateScope.of(context).goalList.length+1,
+        itemBuilder: (BuildContext context, int index) {
+          final randomNumber = Random().nextInt(7);
+          if(index==0) {
+            return Column(
+              children: [
+                GoalTitle(),
+              ],
+            );
+          }
+          else{
+            return Container(
+                height: 80,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  border: Border.all(
+                    color: Colors.lightGreen,
+                    width: 1,
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.all(10.0),
+                      child:  Icon(
+                        Icons.circle,
+                        size: 60,
+                        color: colors[randomNumber],
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(10.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('${AppStateScope.of(context).goalList[index-1].description}',
+                              style: const TextStyle(
+                                fontSize: 15,)
+                          ),
+                          Text('Due date: ${AppStateScope.of(context).goalList[index-1].dueDate}',
+                            style: const TextStyle(
+                              fontSize: 10,)
+                          ),
+                          Row(
+                            children: [
+                              Text('Progress: ${AppStateScope.of(context).goalList[index-1].progress}%',
+                                  style: const TextStyle(
+                                    fontSize: 10,)
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Text('Priority: ${AppStateScope.of(context).goalList[index-1].priority}',
+                                  style: const TextStyle(
+                                    fontSize: 10,)
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+
+                  ],
+                )
+            );}
+        },
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+      );
+  }
+}
+class GoalTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          'Active Goals',
+          style: const TextStyle(
+            fontSize: 30,
+            color: Colors.blue,
+          ),
+        ),
+        Icon(Icons.event_note,
+          size: 40,
+          color: Colors.blue,
+        ),
+        const SizedBox(
+          height: 60.0,
+        ),
+      ],
+    );
+  }
+}
+
+
+
